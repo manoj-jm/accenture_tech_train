f9cb1ddbb59dce6aeb48f692a9e4812c9561a1e3
manoj <jmanojjmvr49@gmail.com>
2024-08-27 12:43:48 +0530

    extend

--- supermarket.py
+++ supermarket.py
@@ -12,16 +12,16 @@ print(o)
 #problem 2
 
 # the sprice of the product is su o fthe numbers in the code  ( take the consecutive numbwer and add them )
-ss = []
+ss = 0
 t = 0
 for i in s:
   if i.isdigit():
     t= t*10 + int(i)
   else:
       if t>0:
-        ss.append(t)
+        ss+=t
       t=0
-if t>0:
-  ss.append(t)
+
+ss+=t
 print(ss)
-print(sum(ss))
+
