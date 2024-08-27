b48063365a7da8f35ebb09cbedf7e04cd465cca3
manoj <jmanojjmvr49@gmail.com>
2024-08-27 14:44:07 +0530

    alternatestring

--- merge2stralernate.py
+++ merge2stralernate.py
@@ -1,21 +1,15 @@
 s1 = input()
 s2 = input()
 i=0
-j=0
-f=1
 ns = ''
-while i!=len(s1) and j!=len(s2):
-  if f==1:
+while i<len(s1) or i<len(s2):
+  if i<len(s1):
     ns+=s1[i]
-    i+=1
-    f=0
-  else:
-    ns+=s2[j]
-    j+=1
-    f=1
+  if i<len(s2):
+    ns+=s2[i]
+  i+=1
 
-
-ns += s1[i:]
-ns +=s2[j:]
+ns +=s1[i:]
+ns +=s2[i:]
 
 print(ns.upper())
